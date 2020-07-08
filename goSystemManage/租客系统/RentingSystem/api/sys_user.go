189c36cdbfc816f9a31c77c692efd241e45807a3
huhy <huhy@cadg.cn>
2020-07-08 10:40:41 +0800

    提交文件信息

--- goSystemManage/租客系统/RentingSystem/api/sys_user.go
+++ goSystemManage/租客系统/RentingSystem/api/sys_user.go
@@ -16,7 +16,13 @@ func GetUserList(c *gin.Context){
         response.OkWithData(list,c);
     }
 }
-
+//@Tags sysUser
+//@Summary 删除用户
+//@accept application/json
+//@produce application/json
+//@Param data body `request.GetById` 删除用户
+//@Success 200 (string) string "{"success":true,"data":{},"message":"修改成功"}"
+//@Router /user/deleteUser
 func DeleteUser(c *gin.Context){
     var reqId request.GetById
     err := service.DeleteUser(reqId.Id)
