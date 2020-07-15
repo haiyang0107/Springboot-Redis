d8df941f44f0d0758898b45d91f7eec9ab7a97a9
huhy <huhy@cadg.cn>
2020-07-15 14:24:05 +0800

    提交spring boot 整合mybatis-添加事务处理

--- springboot-mybatis-config/src/main/java/com/springboot/configdemo/service/DictService.java
+++ springboot-mybatis-config/src/main/java/com/springboot/configdemo/service/DictService.java
@@ -3,27 +3,32 @@ package com.springboot.configdemo.service;
 import com.springboot.configdemo.dao.second.DictMapper;
 import com.springboot.configdemo.entity.master.User;
 import com.springboot.configdemo.entity.second.Dict;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)//开启事务注解
 public class DictService {
     @Autowired
     private DictMapper mapper;
     public List<Dict> findList() {
         return mapper.findList();
     }
-
+    @SneakyThrows
+    @Transactional(readOnly = false)
     public Dict insertDict(Dict dict) {
         int i = mapper.insertDict(dict);
+        System.out.println("确认插入数据条数为：》》》》"+i);
         if(i<= 0){
             return  null;
         }
         return  dict;
     }
-
+    @Transactional(readOnly = false)
     public boolean deleteDict(int id) {
         int i = mapper.deleteDict(id);
         if(i <= 0 ){
